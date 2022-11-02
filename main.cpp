4e91c25386e08959409ac701b942ebba103d7a0c
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-11-02 21:25:05 +0530

    Created the interface.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    char operation;
+    cout << "First Digit : ";
+    cin >> num1;
+
+    cout << "Operation (+ , - , * , /) : ";
+    cin >> operation;
+
+    cout << "Second Digit : ";
+    cin >> num2;
+    return 0;
+}
